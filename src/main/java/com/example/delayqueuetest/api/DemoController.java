9bed345c78824a7fb0892d8d3cb7721a4580b7bf
dz <13484927420@163.com>
2019-01-24 22:43:43 +0800

    mysql密码修改

--- src/main/java/com/example/delayqueuetest/api/DemoController.java
+++ src/main/java/com/example/delayqueuetest/api/DemoController.java
@@ -2,6 +2,8 @@ package com.example.delayqueuetest.api;
 
 import com.example.delayqueuetest.mapper.DemoMapper;
 import com.example.delayqueuetest.model.Demo;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,11 @@ public class DemoController {
     public List<Demo> getList(){
         return demoMapper.getList();
     }
-
+    @RequestMapping(value = "listpage")
+    public PageInfo<Demo> getApps() {
+        PageHelper.startPage(2, 1);
+        List<Demo> appsList = demoMapper.getList();
+        PageInfo<Demo> appsPageInfo = new PageInfo<>(appsList);
+        return appsPageInfo;
+    }
 }
