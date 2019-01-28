40deb18cf11a54b4d048c5480c64f1f4894e447c
董茁 <dongz001928@yimidida.com>
2019-01-28 09:01:07 +0800

    先屏蔽点rabbitmq

--- src/main/java/com/example/delayqueuetest/api/DemoController.java
+++ src/main/java/com/example/delayqueuetest/api/DemoController.java
@@ -2,11 +2,13 @@ package com.example.delayqueuetest.api;
 
 import com.example.delayqueuetest.mapper.DemoMapper;
 import com.example.delayqueuetest.model.Demo;
+import com.example.delayqueuetest.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -15,8 +17,12 @@ public class DemoController {
     @Autowired
     private DemoMapper demoMapper;
 
+    @Autowired
+    private RedisService redisService;
+
     @RequestMapping("/lists")
     public List<Demo> getList(){
+        redisService.set(UUID.randomUUID().toString(),demoMapper.getList(),20L);
         return demoMapper.getList();
     }
 
